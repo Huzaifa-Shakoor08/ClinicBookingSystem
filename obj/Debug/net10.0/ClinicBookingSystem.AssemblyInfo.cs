d08cec8bd6ad8022e644dfcc6273102f0af2a700
Huzaifa Shakoor <huzaifshakoor08@gmail.com>
2026-06-27 16:38:53 +0500

    Fix nuget fallback folders for Linux deployment

--- obj/Debug/net10.0/ClinicBookingSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/ClinicBookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f593617d7a294cc7d18a95e5e3c3d43a1d5f1f0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3780a5f5cb633e5b4b68408e40cf537fabdd324")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
